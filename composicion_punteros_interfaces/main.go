62e2785402a41b114ce9e71f6ba40e11772eb9ce
EmiiFernandez <e.fernandezmurgia@gmail.com>
2024-04-02 20:07:38 -0300

    pointers

--- composicion_punteros_interfaces/main.go
+++ composicion_punteros_interfaces/main.go
@@ -8,9 +8,8 @@ import (
 func main() {
 	fmt.Println("Main")
 	/* ******************* 	Composición, punteros e interfaces ******************* */
-
 	fmt.Println(`
-	******************* Punteros *******************
+	******************* Ejemplo sin y con punteros *******************
 	`)
 
 	// Declarar una variable
@@ -19,9 +18,40 @@ func main() {
 	fmt.Println("Dirección de memoria de var fuera de la función increment: ", &myVar)
 	// Llamar a la función increment y pasarle la variable por valor
 
+	fmt.Println(`
+	******************* Punteros *******************
+	`)
+
+	var i int
+	var iP *int
+
+	i = 34
+	iP = &i
+
+	fmt.Println("Dirección memoría variable i: ", &i)
+	fmt.Println("Puntero: ", iP)
+	fmt.Println("Dirección memoría del puntero: ", &iP)
+	fmt.Println("Variable i: ", i)
+	fmt.Println("Valor del puntero: ", *iP)
+
+	//Modifico el valor de iP
+	*iP = 1
+
+	//Cuando modifico uno se modifica el otro
+	fmt.Printf("value i: %d, value pointer i: %d\n", i, *iP)
+
+	fmt.Println()
+
+	myVar = 30
+	fmt.Printf("address: %p, value: %d\n", &myVar, myVar)
+
+	fmt.Println()
+
 	pointers.Increment(myVar)
 
 	fmt.Println("Dirección de memoria de var fuera de la función increment: ", &myVar)
+
+	fmt.Println()
 
 	pointers.IncrementP(&myVar) // Llama a la función IncrementP pasando un puntero a myVar
 
