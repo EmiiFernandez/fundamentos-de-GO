3fff71fa4d080eefee1e616d3ce91f8e8a1fb147
EmiiFernandez <e.fernandezmurgia@gmail.com>
2024-04-06 19:26:43 -0300

    defer/panic/recover

--- errors/main.go
+++ errors/main.go
@@ -78,27 +78,51 @@ func main() {
 	fmt.Println(errors.Unwrap(errors.Unwrap(err7)))
 	fmt.Println(errors.Unwrap(errors.Unwrap(errors.Unwrap(err7))))
 
+	/*
+	   errors.New: Crear un nuevo error con un mensaje específico. Retorna un valor de tipo error. Por ejemplo:
+	   err := errors.New("este es un mensaje de error")
+
+	   errors.Is: Verificar si un error es igual a otro error. Retorna un valor booleano que indica si el error dado es igual al error especificado. Se utiliza para manejar errores específicos en el código. Por ejemplo:
+	   if errors.Is(err1, err2) {
+	       // manejar el error
+	   }
+
+	   errors.Join: Combinar varios errores en uno solo. Retorna un error que contiene la concatenación de los mensajes de error de los errores dados. Se utiliza para agrupar múltiples errores en un solo error. Por ejemplo:
+	   err := errors.Join(err1, err2, err3)
+
+	   errors.Unwrap: Se utiliza para desenvolver un error que ha sido envuelto previamente. Retorna el error envuelto original que se pasó a la función fmt.Errorf con el especificador %w. Se utiliza para extraer el error original de una cadena de errores envueltos. Por ejemplo:
+	   originalErr := errors.Unwrap(wrappedErr)
+	*/
+
 	fmt.Println(`
-	---------------- Defer ----------------
+	---------------- Defer / Panic / Recover ----------------
 	`)
 
-	//Define algo que se va a ejecutar al final del programa
-	//Nos permite recuperar errores
-
+	// Define una función defer que se ejecutará al final del programa
+	// Permite recuperar errores que puedan ocurrir durante la ejecución
 	defer func() {
-		fmt.Println("end main") // Se ejecuta al final
-		r := recover()          //Error que se haya generado
+		fmt.Println("end main") // Se ejecuta al final del programa
+		r := recover()          // Recupera cualquier error que se haya producido (si lo hay)
 		if r != nil {
-			fmt.Println("Recover in ", r)
+			fmt.Println("Recover in ", r) // Maneja y muestra el error recuperado en caso de panic
 		}
 	}()
 
-	//Dispara un error
-	//Se pone detras del defer
-	//panic("my panic")
-	mypackage.Run()
+	// Dispara un error
+	// Se coloca detrás del defer para asegurarse de que el defer se ejecute antes de que se produzca el panic
+	// panic("my panic")
+	mypackage.Run() // Llama a una función que puede generar un panic
 
-	fmt.Println("end main") //Se ejecuta antes del end
+	fmt.Println("end main") // Se ejecuta antes de "end main"
+
+	/*
+	   panic: Se utiliza para generar un error fatal. Cuando se encuentra un panic, la ejecución del programa se detiene inmediatamente y se ejecuta cualquier función defer pendiente antes de salir del programa. El mensaje pasado a panic puede ser cualquier valor, no solo una cadena.
+
+	   defer: Se utiliza para posponer la ejecución de una función hasta que la función que la rodea haya terminado de ejecutarse. Las funciones defer se ejecutan en el orden inverso en el que se definen, es decir, la última en ser definida será la primera en ser ejecutada.
+
+	   recover: Se utiliza para recuperar y manejar un pánic. Se coloca dentro de una función defer. Si no hay ningún panic, recover devuelve nil. Si se llama a recover dentro de una función defer y la función circundante ha generado un panic, recover detiene la propagación del panic y devuelve el valor pasado a panic. Esto permite que el programa continúe su ejecución después de un panic y proporciona una manera de manejarlo de manera controlada.
+	*/
+
 }
 
 // TestError es una función que devuelve un error personalizado según el valor pasado
--- errors/mypackage/package.go
+++ errors/mypackage/package.go
@@ -2,13 +2,14 @@ package mypackage
 
 import "fmt"
 
+// La función Run es responsable de manejar cualquier pánico que pueda ocurrir dentro de ella
 func Run() {
 	defer func() {
-		println("end my function")
-		r := recover()
+		println("end my function") // Se ejecuta al final de la función
+		r := recover()             // Recupera cualquier pánico que ocurra dentro de la función
 		if r != nil {
-			fmt.Println("error in run: ", r)
+			fmt.Println("error in run: ", r) // Maneja y muestra el error recuperado
 		}
 	}()
-	panic("panic in run function")
+	panic("panic in run function") // Genera un pánico dentro de la función
 }
