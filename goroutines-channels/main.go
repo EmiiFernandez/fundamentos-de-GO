bc96cfa5ebcdcced94c90e4e5340db5ec49a6bce
EmiiFernandez <e.fernandezmurgia@gmail.com>
2024-04-14 21:43:55 -0300

    mod goroutines-channels

--- goroutines-channels/main.go
+++ goroutines-channels/main.go
@@ -5,15 +5,6 @@ import (
 	"time"
 )
 
-/*
- Goroutines son funciones que se ejecutan concurrentemente con otras goroutines.
- Son ligeros hilos de ejecución que son administrados por el runtime de Go,
- permitiendo la ejecución concurrente y paralela en programas.
-
-Los channels son un mecanismo de comunicación que permite la sincronización y la comunicación entre goroutines en Go.
-Se pueden utilizar para enviar y recibir valores de un extremo a otro, lo que permite la coordinación entre goroutines.
-*/
-
 func main() {
 	//Se ejecuta A y luego B de manera secuencial (sin goroutines)
 	fmt.Println(`
@@ -118,3 +109,22 @@ func myOtherProcessWithChannel(p string, c chan string) {
 	// Enviamos un mensaje al canal para indicar que el proceso ha terminado
 	c <- "ok"
 }
+
+/*
+Goroutines:
+
+Es una función que se ejecuta de manera concurrente con otras goroutines dentro del mismo proceso. Es una forma de lograr la concurrencia en Go (capacidad de ejecutar múltiples tareas de manera simultánea dentro de un mismo programa), permitiendo que varias funciones se ejecuten simultáneamente de manera eficiente y sincronizada.
+
+Son ligeras en términos de consumo de recursos y pueden ser creadas y manejadas fácilmente. Se pueden pensar como hilos de ejecución, pero gestionadas por el runtime de Go en lugar del sistema operativo.
+
+Son útiles cuando necesitas realizar operaciones simultáneas sin bloquear el flujo principal del programa. Por ejemplo, cuando se trabaja con operaciones de entrada/salida (I/O) intensivas, como solicitudes HTTP, acceso a bases de datos o lectura/escritura de archivos, las goroutines permiten que el programa continúe ejecutándose mientras espera las respuestas de estas operaciones.
+
+Channels:
+
+Son una forma de comunicación y sincronización entre goroutines. Permiten que las goroutines envíen datos de un lado a otro de manera segura y coordinada, evitando así las condiciones de carrera y los problemas de sincronización comunes en la programación concurrente.
+
+Se puede pensar como un conducto por el cual las goroutines pueden enviar y recibir valores de un tipo específico. Pueden ser utilizados para coordinar el flujo de datos entre goroutines, permitiendo la comunicación asíncrona y la sincronización de tareas.
+
+Son útiles en escenarios donde se necesita compartir datos entre goroutines de manera segura y eficiente, como la comunicación entre productores y consumidores, la implementación de patrones de concurrencia como el pool de workers, o la coordinación de tareas en un programa concurrente.
+
+La concurrencia en Go se logra principalmente mediante goroutines y canales. Las goroutines son funciones que se ejecutan de manera concurrente, mientras que los canales son medios de comunicación que permiten la sincronización y la transferencia de datos entre goroutine*/
